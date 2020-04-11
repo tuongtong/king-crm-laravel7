e07c662639c32ae0055dff884826a32ef5f6e7ed
Phat Nham <phattannham@gmail.com>
2020-04-11 23:54:37 +0700

    fix: added leader and manager permission

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'staff' => \App\Http\Middleware\StaffAuthenticate::class,
+        'leader' => \App\Http\Middleware\LeaderPermission::class,
+        'manager' => \App\Http\Middleware\ManagerPermission::class,
     ];
 }
---
+++ app/Http/Middleware/LeaderPermission.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class LeaderPermission
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next,  $guard = 'staff')
+    {
+        if (Auth::guard($guard)->user()->level < 2) {
+            return redirect()->back()->withErrors('Bạn không có quyền truy cập, hãy liên hệ với cấp quản lý.');
+        }
+
+        return $next($request);
+    }
+}
