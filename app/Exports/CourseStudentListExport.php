90664ce9dcab186e0b71002ba4d0f3913e71645e
Phat Nham <phattannham@gmail.com>
2020-08-19 13:57:02 +0700

    fix: export course list by first name

--- app/Exports/CourseStudentListExport.php
+++ app/Exports/CourseStudentListExport.php
@@ -14,15 +14,21 @@ class CourseStudentListExport implements FromCollection, WithHeadings
     
     public function __construct($course)
     {
-        $students = $course->courseStudents;
+        $students = $course->courseStudents->map(function ($item, $key) {
+            $item->client->first_name = array_slice(explode(' ', $item->client->name), -1)[0];
+            return $item;
+        })->sortBy('client.first_name');
+
         $student_arr = array(array());
-        foreach ($students as $i => $student) {
+        $i = 0;
+        foreach ($students as $student) {
             $student_arr[$i]['name'] = $student->client->name;
             $student_arr[$i]['phone'] = $student->client->phone;
             $student_arr[$i]['email'] = $student->client->email;
             $student_arr[$i]['deal_rate'] = $student->deal_rate;
             $student_arr[$i]['deal_note'] = $student->deal_note;
             $student_arr[$i]['tuition_done'] = $student->tuition_done;
+            $i++;
         }
 
         $this->student_arr = $student_arr;
